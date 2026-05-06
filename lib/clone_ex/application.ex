1d0171928706164e4f6e87770b7b2d2d6fbb2fd7
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-05-06 23:42:58 +0530

    the core, application

--- lib/clone_ex/application.ex
+++ lib/clone_ex/application.ex
@@ -1,6 +1,4 @@
 defmodule CloneEx.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,26 @@ defmodule CloneEx.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: CloneEx.Worker.start_link(arg)
-      # {CloneEx.Worker, arg}
+      # HTTP/2 connection pool for GitHub API
+      {Finch,
+       name: CloneEx.Finch,
+       pools: %{
+         "https://api.github.com" => [
+           size: 10,
+           count: 1,
+           protocols: [:http2]
+         ]
+       }},
+
+      # Supervised task runner for git clone subprocesses.
+      # Ensures clean shutdown: all running clones are terminated when the
+      # application stops, preventing orphaned `git` OS processes.
+      {Task.Supervisor, name: CloneEx.TaskSupervisor},
+
+      # Telemetry event handler (attaches to :telemetry on init)
+      CloneEx.Telemetry
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CloneEx.Supervisor]
     Supervisor.start_link(children, opts)
   end
